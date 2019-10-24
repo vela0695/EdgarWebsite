a1e0c8c6d8ca4f0932a6e0bb198acfd43d4735cb
Edgar Velazquez <vela0695@vandals.uidaho.edu>
2019-10-23 19:52:17 -0700

    Added my new picture

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Element, animateScroll as scroll } from "react-scroll";
-import Edgar from "./images/Edgar.jpg";
+import Edgar from "./images/Edgar.jpeg";
 
 class About extends React.Component {
   state = {};
